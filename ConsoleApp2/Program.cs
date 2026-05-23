097d75b8d9f3bfc767213ca9041952aedf3f3159
Abhia <Abhia@ECHAN>
2026-05-23 20:48:40 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Octokit;
+
+var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+var user = await github.User.Get("abhitrueprogrammer");
+Console.WriteLine(user.Followers + $" folks love {user.Login}!");
